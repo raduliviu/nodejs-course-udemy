fb949ffd3ae67b934365267000c751f5ebb709e5
Radu Baltag <baltag.radu@gmail.com>
2023-09-25 14:40:32 +0200

    feat: getting and posting products to sql db

---
+++ util/database.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'node-complete',
+  password: process.env.DB_PASSWORD,
+});
+
+module.exports = pool.promise();
