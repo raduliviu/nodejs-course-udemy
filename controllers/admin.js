e6c39e256ff4006ba257834e6d37b887a2f0d2c3
Radu Baltag <baltag.radu@gmail.com>
2023-09-27 15:24:54 +0200

    feat: posting new product through sqlize

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
-    })
+  Product.create({
+    title,
+    price,
+    imageUrl,
+    description,
+  })
+    .then(console.log('Created Product'))
     .catch((err) => console.log(err));
-  res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
