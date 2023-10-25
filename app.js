f9d7ec6838c6ff57b54f75abb150b0f1fe39e0d0
Radu Baltag <baltag.radu@gmail.com>
2023-10-25 11:57:03 +0200

    feat: added password encryption

--- app.js
+++ app.js
@@ -54,16 +54,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: 'Radu',
-          email: 'radu@test.com',
-          cart: { items: [] },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => console.log(err));
