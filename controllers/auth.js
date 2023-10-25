950f56ec3c131f78c86436f389b92095c7e16103
Radu Baltag <baltag.radu@gmail.com>
2023-10-25 11:36:10 +0200

    feat: added signup page

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,8 +8,16 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res, next) => {
-  User.findById('65212c928cd2c212fd37563c')
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
@@ -20,6 +28,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
