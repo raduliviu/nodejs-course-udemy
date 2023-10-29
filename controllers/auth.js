8d6019841abcf5558a65914b37b4b04d069789d0
Radu Baltag <baltag.radu@gmail.com>
2023-10-29 12:56:02 +0100

    feat: nodemailer setup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,19 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  },
+});
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -87,6 +100,22 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect('/login');
+          return transporter.sendMail(
+            {
+              from: `${process.env.MAIL_USERNAME}`,
+              to: email,
+              subject: 'Signup succeeded!',
+              text: 'You successfully signed up!',
+              html: '<h1>You successfully signed up!</h1>',
+            },
+            function (err, data) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log('Email sent successfully');
+              }
+            }
+          );
         });
     })
     .catch((err) => console.log(err));
