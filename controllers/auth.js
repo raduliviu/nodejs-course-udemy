a208dbf900baa30770cd212aeaac4e9ed81cebe0
Radu Baltag <baltag.radu@gmail.com>
2023-11-06 14:55:00 +0100

    feat: signup page validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const crypto = require('crypto');
 
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -79,46 +80,43 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
-
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash(
-          'error',
-          'E-Mail exists already, please pick a different one.'
-        );
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] },
-          });
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect('/login');
-          return transporter.sendMail(
-            {
-              from: `${process.env.MAIL_USERNAME}`,
-              to: email,
-              subject: 'Signup succeeded!',
-              text: 'You successfully signed up!',
-              html: '<h1>You successfully signed up!</h1>',
-            },
-            function (err, data) {
-              if (err) {
-                console.log(err);
-              } else {
-                console.log('Email sent successfully');
-              }
-            }
-          );
-        });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect('/login');
+      return transporter.sendMail(
+        {
+          from: `${process.env.MAIL_USERNAME}`,
+          to: email,
+          subject: 'Signup succeeded!',
+          text: 'You successfully signed up!',
+          html: '<h1>You successfully signed up!</h1>',
+        },
+        function (err, data) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('Email sent successfully');
+          }
+        }
+      );
     })
     .catch((err) => console.log(err));
 };
