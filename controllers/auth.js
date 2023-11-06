27cb98c3a208e38073f714ac490ce9a57f4088f1
Radu Baltag <baltag.radu@gmail.com>
2023-11-06 15:17:42 +0100

    feat: validating login page form

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,6 +49,17 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+
   User.findOne({ email })
     .then((user) => {
       if (!user) {
