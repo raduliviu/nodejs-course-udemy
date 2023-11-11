bea32b902524b32322b9d4efb00053c53d573213
Radu Baltag <baltag.radu@gmail.com>
2023-11-11 12:33:32 +0100

    feat: stripe integration quickstart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
+const stripe = require('stripe')(`${process.env.STRIPE_SECRET}`);
 
 const PDFDocument = require('pdfkit');
 
@@ -133,20 +135,73 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
   req.user
     .populate('cart.items.productId')
     .then((user) => {
-      const products = user.cart.items;
-      let total = 0;
+      products = user.cart.items;
       products.forEach((p) => {
-        total += p.quantity * p.productId.price;
+        total += +p.quantity * +p.productId.price;
       });
+
+      return stripe.checkout.sessions.create({
+        line_items: products.map((p) => {
+          return {
+            price_data: {
+              currency: 'eur',
+              unit_amount: parseInt(Math.ceil(p.productId.price * 100)),
+              product_data: {
+                name: p.productId.title,
+                description: p.productId.description,
+              },
+            },
+            quantity: p.quantity,
+          };
+        }),
+        mode: 'payment',
+        success_url:
+          req.protocol + '://' + req.get('host') + '/checkout/success',
+        cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
+      });
+    })
+    .then((session) => {
       res.render('shop/checkout', {
         path: '/checkout',
         pageTitle: 'Checkout',
         products,
-        totalSum: total,
+        totalSum: total.toFixed(2),
+        sessionId: session.id,
       });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect('/orders');
     })
     .catch((err) => {
       const error = new Error(err);
--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const express = require('express');
 
 const shopController = require('../controllers/shop');
@@ -20,6 +18,10 @@ router.post('/cart', isAuth, shopController.postCart);
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
 router.get('/checkout', isAuth, shopController.getCheckout);
+
+router.get('/checkout/success', shopController.getCheckoutSuccess);
+
+router.get('/checkout/cancel', shopController.getCheckout);
 
 router.post('/create-order', isAuth, shopController.postOrder);
 
